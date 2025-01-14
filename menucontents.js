f784091a54122c4d27826b5648f92512a896954d
nareshvarodiya <148626868+nareshvarodiya@users.noreply.github.com>
2025-01-14 15:59:39 +0530

    first commit

---
+++ menucontents.js
@@ -0,0 +1,32 @@
+var anylinkmenu1={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu1.items=[	
+	//["Chetak Samrat Pluse", "product.html#smartplus"],
+	["Chetak Star Pluse", "product.html#starplus"],
+	["Chetak Pluto", "product.html#pluto"],
+	["Chetak Mercury", "product.html#mercury"],
+	["Chetak Jupiter", "product.html#jupiter"],
+
+	["Chetak 500Lph RO Plant", "product.html#500Lph"],
+	["Chetak 1000Lph RO Plant", "product.html#1000Lph"],
+	["Chetak 6000Lph RO Plant", "product.html#6000Lph"]
+	
+	
+	
+	
+		
+	//no comma following last entry!
+]
+
+var anylinkmenu2={divclass:'anylinkmenu', inlinestyle:'', linktarget:''} //First menu variable. Make sure "anylinkmenu1" is a unique name!
+anylinkmenu2.items=[
+	["Water Softner (1054)", "product_ws.html#ws_1054"],
+	["Water Softner (1248)", "product_ws.html#ws_1248"],
+	["Water Softner (1354)", "product_ws.html#ws_1354"],
+	["Water Softner (1465)", "product_ws.html#ws_1465"],
+
+	["Water Softner (1665)", "product_ws.html#ws_1665"],
+	["Water Softner (2472)", "product_ws.html#ws_2472"],
+	["Water Softner (2472)", "product_ws.html#ws_3672"],
+	["20 Bag & Sand Filter", "product_ws.html#bag-sand-filter20"]//no comma following last entry!
+]
+
